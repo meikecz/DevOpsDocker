0ddd1961e6e842e1aa970a959e3603ae87fdce4b
meikecz <m.zehnder@gmx.net>
2024-05-09 22:54:27 +0200

    update meike 2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2024 DevOps Course! Das ist Meikes');
+	res.send('Hello FS2024 DevOps Course! Das ist Meikes 2:) ');
 });
 
 app.listen(PORT, HOST);
